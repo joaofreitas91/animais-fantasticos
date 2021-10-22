49d62e23952509ee22efd22294add928e9434a1b
Joao Paulo Freita <jp.contatoprofissional@gmail.com>
2021-10-22 11:51:47 -0300

    tab navegation

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// Retorne no console todas as imagens do site
-console.log(document.querySelectorAll('img'));
-console.log(document.getElementsByTagName('img'));
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-console.log(document.querySelectorAll('img[src^="img/ima"]'));
+if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active');
 
-// Selecione todos os links internos (onde o href começa com #)
-console.log(document.querySelectorAll('[href^="#"]'));
+    tabMenu.forEach((li, index) => {
+        li.addEventListener('click', () => {
+            activeTab(index);
+        });
+    });
 
-// Selecione o primeiro h2 dentro de .animais-descricao
-console.log(document.querySelector('.animias-descricao h2'));
-
-// Selecione o último p do site
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('active');
+        })
+        tabContent[index].classList.add('active');
+    }
+}
 
